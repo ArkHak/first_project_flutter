1dd2b1ad69459701546b704bbc360b1ca3d11443
Oleg Mysin <7nacu7@gmail.com>
2024-07-30 11:56:06 +0300

    step 5  - completed vehicle_item.dart;

--- lib/pages/vehicle/vehicle_item.dart
+++ lib/pages/vehicle/vehicle_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../../design/colors.dart';
 import '../../design/dimensions.dart';
@@ -9,19 +10,22 @@ class VehicleItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: surfaceColor,
-      margin: EdgeInsets.zero,
-      elevation: elevation006,
-      shape:
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius8)),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(radius8),
-        onTap: () {},
-        child: Padding(
-          padding: const EdgeInsets.only(left: padding8, right: padding16),
-          child: Row(
-            children: <Widget>[vehicleMotorcycleImages, _title(), _state()],
+    return SizedBox(
+      height: height64,
+      child: Card(
+        color: surfaceColor,
+        margin: EdgeInsets.zero,
+        elevation: elevation006,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(radius8)),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(radius8),
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.only(left: padding8, right: padding16),
+            child: Row(
+              children: <Widget>[vehicleMotorcycleImages, _title(), _state()],
+            ),
           ),
         ),
       ),
@@ -29,10 +33,72 @@ class VehicleItem extends StatelessWidget {
   }
 
   Widget _title() {
-    return Container();
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: padding6, right: padding6),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "BWM M3 SUPER",
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: secondaryColor,
+                  fontSize: fontSize14,
+                  fontWeight: FontWeight.w600),
+            ),
+            if (false)
+              Text(
+                "No Driver",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    color: secondaryVariantColor,
+                    fontSize: fontSize14,
+                    fontWeight: FontWeight.w400),
+              )
+            else
+              RichText(
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                text: const TextSpan(
+                    style: TextStyle(fontSize: fontSize14),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: "Driver",
+                          style: TextStyle(
+                              color: secondaryVariantColor,
+                              fontWeight: FontWeight.w400)),
+                      TextSpan(
+                          text: "Maximus",
+                          style: TextStyle(
+                              color: secondaryColor,
+                              fontWeight: FontWeight.w600))
+                    ]),
+              )
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _state() {
-    return Container();
+    return InkWell(
+      onTap: () {},
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            statePickupImages,
+            const Text(
+              "pickup",
+              style: TextStyle(
+                  color: secondaryVariantColor,
+                  fontSize: fontSize12,
+                  fontWeight: FontWeight.w400),
+            )
+          ]),
+    );
   }
 }
