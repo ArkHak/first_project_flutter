46201fefd5241511fb4e5db5abd5369be7a0e6f4
Oleg Mysin <7nacu7@gmail.com>
2024-07-30 10:11:40 +0300

    step 1  - add assets/images;  - add colors;  - first setup, cleaning;

---
+++ lib/design/images.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+final SvgPicture accountCircleImages =
+    SvgPicture.asset('assets/images/account_circle.svg');
+final SvgPicture arrowBackImages =
+    SvgPicture.asset('assets/images/arrow_back.svg');
+final SvgPicture checkImages = SvgPicture.asset('assets/images/check.svg');
+
+final SvgPicture stateBoardingImages =
+    SvgPicture.asset('assets/images/state_boarding.svg');
+final SvgPicture stateDeliveryImages =
+    SvgPicture.asset('assets/images/state_delivery.svg');
+final SvgPicture stateLoadImages =
+    SvgPicture.asset('assets/images/state_load.svg');
+final SvgPicture stateParkingImages =
+    SvgPicture.asset('assets/images/state_parking.svg');
+final SvgPicture statePickupImages =
+    SvgPicture.asset('assets/images/state_pickup.svg');
+final SvgPicture stateRepairImages =
+    SvgPicture.asset('assets/images/state_repair.svg');
+final SvgPicture stateUnboardingImages =
+    SvgPicture.asset('assets/images/state_unboarding.svg');
+final SvgPicture stateUnloadImages =
+    SvgPicture.asset('assets/images/state_unload.svg');
+
+final SvgPicture vehicleBusImages =
+    SvgPicture.asset('assets/images/vehicle_bus.svg');
+final SvgPicture vehicleCarImages =
+    SvgPicture.asset('assets/images/vehicle_car.svg');
+final SvgPicture vehicleMotorcycleImages =
+    SvgPicture.asset('assets/images/vehicle_motorcycle.svg');
+final SvgPicture vehicleTruckImages =
+    SvgPicture.asset('assets/images/vehicle_truck.svg');
+final SvgPicture vehicleTuktukImages =
+    SvgPicture.asset('assets/images/vehicle_tuktuk.svg');
